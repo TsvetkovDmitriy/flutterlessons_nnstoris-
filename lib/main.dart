c40087c5ba28c9e3909767215c4402003a0117d8
TsvetkovDmitriy <Dmitriy-37>
2022-05-13 00:03:28 +0300

    первый коммит урок 4

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:nnstoris_lesson/second_screen.dart';
 
 void main() {
   runApp(const MyApp());
@@ -51,6 +52,7 @@ class _MyHomePageState extends State<MyHomePage> {
   int _counter = 0;
 
   void _incrementCounter() {
+    createUser(timeOffBirthday: DateTime.now(), phone:'+79998887766', name:'Иван');
     setState(() {
       // This call to setState tells the Flutter framework that something has
       // changed in this State, which causes it to rerun the build method below
--- lib/second_screen.dart
+++ lib/second_screen.dart
@@ -5,8 +5,28 @@ class User {
   bool online = false;
   DateTime lastSignIn = DateTime.now();
 
+  void signIn (){
+    online = true;
+    lastSignIn = DateTime.now();
+  }
 
+  void signUp (){
+    print ('Пользователь $name успешно зарегистрирован');
+  }
+}
 
+void createUser ({String name="Пользователь", String phone="", required DateTime timeOffBirthday}) {
+  User newUser = User();
+  newUser.name = name;
+  newUser.phone = phone;
+  // print ('Имя - ' + newUser.name);
+  // print ('Телефон - ' + newUser.phone);
+  newUser.signUp();
 
-
+  User newUser2 = User();
+  newUser2.name = 'Федор';
+  newUser2.phone = "+79877898899";
+  // print ('Имя - ' + newUser.name);
+  // print ('Телефон - ' + newUser.phone);
+  newUser2.signUp();
 }
